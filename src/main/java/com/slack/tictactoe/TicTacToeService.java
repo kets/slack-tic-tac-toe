f0af427c5cb557e949fa3428a1abbb1901129272
Ketaki Borkar <toesoxluver@gmail.com>
2017-03-01 12:26:17 -0800

    added POST endpoint to process ttt command from slack

--- src/main/java/com/slack/tictactoe/TicTacToeService.java
+++ src/main/java/com/slack/tictactoe/TicTacToeService.java
@@ -1,12 +1,13 @@
 package com.slack.tictactoe;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
-@Path("/slack-ttt/")
+@Path("/tictactoe/")
 @Produces(MediaType.APPLICATION_JSON)
 public class TicTacToeService {
 	@Path("heartbeat")
@@ -14,5 +15,13 @@ public class TicTacToeService {
 	public Response helloWorld() {
 		return Response.status(Response.Status.OK).entity("hello world!").build();
 	}
+	
+	@Path("ttt")
+	@POST
+	public Response processTTTCommand(String body) {
+		
+		return Response.status(Response.Status.OK).entity("Got the command!").build();
+	}
+	
 
 }

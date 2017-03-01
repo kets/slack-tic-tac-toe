ba3db607a7fd8eee5e4e27f5a8c00133bbd8e683
Ketaki Borkar <toesoxluver@gmail.com>
2017-02-28 23:11:35 -0800

    adding and fixing mvn dependencies for jetty

--- src/main/java/com/slack/tictactoe/TicTacToeService.java
+++ src/main/java/com/slack/tictactoe/TicTacToeService.java
@@ -2,9 +2,12 @@ package com.slack.tictactoe;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 @Path("/slack-ttt/")
+@Produces(MediaType.APPLICATION_JSON)
 public class TicTacToeService {
 	@GET
 	public Response helloWorld(String name) {

bd040c7a9473a9726975a095a09b966395e03c8a
Ketaki Borkar <toesoxluver@gmail.com>
2017-03-01 20:54:57 -0800

    using MultiValueMap for the input form params in the body from Slack

--- src/main/java/com/slack/tictactoe/service/TicTacToeService.java
+++ src/main/java/com/slack/tictactoe/service/TicTacToeService.java
@@ -1,16 +1,20 @@
 package com.slack.tictactoe.service;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 
 @Path("/ttt")
 public class TicTacToeService {
+	private static final Logger logger = LoggerFactory.getLogger(TicTacToeService.class);
+
 	@Path("/heartbeat")
 	@Produces(MediaType.APPLICATION_JSON)
 	@GET
@@ -19,9 +23,10 @@ public class TicTacToeService {
 	}
 	
 	@POST
-	public Response processTTTCommand(String request) {
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response processTTTCommand(MultivaluedMap<String, String> formParams) {
 		
-		return Response.status(Response.Status.OK).entity("Let's play! " +request).build();
+		return Response.status(Response.Status.OK).entity("Let's play! " +formParams.getFirst("username")).build();
 	}
 	
 
